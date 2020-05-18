12c3e5bcb29dd994dd5e766652a1b236f9968df7
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 20:06:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if pigeon_list[name][key] == nil
           pigeon_list[name][key] = []
         end
-        pigeon_list[name][key].push(key2.to_s)
+        pigeon_list[name][key].push(key2)
       end
     end
   end
