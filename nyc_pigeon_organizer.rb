cb6932e4040ac13ef1415ab4143bea02eb1fb3a9
David Lee <david.lee.sl2634@yale.edu>
2020-05-18 20:12:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,5 +4,15 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value| 
     value.each do |key2, array|
       array.each do |name|
-        
+        if pigeon_list[name] == nil 
+          pigeon_list = {}
+        end
+        if pigeon_list[name][key] == nil 
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key] = 
+      end
+    end
+  end
+end
 
